fc861e2d4bfdcfe2c5df970093654b5c54afcdf3
Mateis <54773138+mateis-rgb@users.noreply.github.com>
2023-02-20 22:11:26 +0100

    Add files via upload

---
+++ client/main.js
@@ -0,0 +1,64 @@
+const friend_list = document.querySelector('#friends-list');
+const search_friend = document.querySelector("#search-friend");
+
+const friends = [
+    { name: "John Doe 3", isFriend: false },
+    { name: "John Doe 0", isFriend: true },
+    { name: "John Doe 4", isFriend: false },
+    { name: "John Doe 1", isFriend: true },
+    { name: "John Doe 2", isFriend: true },
+];
+
+/**
+ * 
+ * @param {Object} friendList 
+ * @param {Element} friendListElement 
+ * @param {String} searchText 
+ */
+function loadFriendListWithSearch (friendList, friendListElement, searchText) {
+    friendListElement.innerHTML = "";
+
+    friendList.forEach((element) => {
+        if (element.name.includes(searchText)) {   
+            let elm = document.createElement("div");
+            
+            if (element.isFriend) {   
+                elm.classList = "list-group-item list-group-item-action px-3 border-0 d-flex align-items-center";
+                elm.innerHTML = `<div class="p-2 flex-grow-1">
+                    ${element.name}
+                </div>
+                <div class="p-2">
+                    <button class="btn btn-primary">
+                        <i class="fa-regular fa-message"></i>
+                    </button>
+                </div>`;
+            }
+            else {
+                elm.classList = "list-group-item list-group-item-action px-3 border-0 d-flex align-items-center";
+                elm.innerHTML = `<div class="p-2 flex-grow-1">
+                    ${element.name}
+                </div>
+                <div class="p-2">
+                    <button class="btn btn-success">
+                        <i class="fa-solid fa-add"></i>
+                    </button>
+                    <button class="btn btn-danger" hidden>
+                        <i class="fa-regular fa-circle-xmark"></i>
+                    </button>
+                </div>`;
+            }
+
+
+
+            friendListElement.appendChild(elm);
+        }
+    });
+}
+
+loadFriendListWithSearch(friends, friend_list, search_friend.value);
+
+search_friend.addEventListener("change", (e) => {
+    e.preventDefault();
+    
+    loadFriendListWithSearch(friends, friend_list, search_friend.value);
+});
